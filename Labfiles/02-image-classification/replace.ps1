0315f02dcc1527cbcb44d1f24c2aa35e6e84f3dd
Meghamily7 <megha.pothamsetty@zionclouds.com>
2024-09-29 04:39:20 +0000

    Pending changes exported from your codespace

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,3 @@
-$storageAcct = '<storageAccount>'
+$storageAcct = '<ai900courseram6486407501
+>'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
